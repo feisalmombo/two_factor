9b3812558206fa92e79753500ece4e6c793d6f01
Feisal Mombo <feisalmombo29@gmail.com>
2020-11-03 17:00:11 +0300

    fix userstableseeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,7 @@ class UsersTableSeeder extends Seeder
             [
                 'id'             => 1,
                 'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
+                'email'          => 'feisalmombo29@yahoo.com',
                 'password'       => '$2y$10$E44UqfAOx8epS0SN2Y3ZvuqXUqRih9xW7UzcDiqzfmOJ5djih9Vq2',
                 'remember_token' => null,
                 'created_at'     => '2019-09-27 07:11:07',
